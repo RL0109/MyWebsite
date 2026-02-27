bf6100de68b809cbb6d997621fa83da174313841
unknown <rwl54@scarletmail.rutgers.edu>
2026-02-26 19:12:16 -0500

    Bug test to see if worker is correctly used.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,15 @@
 export default {
     async fetch(request, env) {
+      // If this prints false, your deployed Worker does NOT have the assets binding
+      const hasAssets = !!env.ASSETS;
+  
+      if (!hasAssets) {
+        return new Response(
+          "ASSETS binding missing. This deploy is not using wrangler assets config.",
+          { status: 500 }
+        );
+      }
+  
       return env.ASSETS.fetch(request);
-    }
-  }
+    },
+  };
